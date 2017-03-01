881f1c5b346fe72803d94989059fe6618ab6e8ad
Yastremsky Vitaly <yastremsky@gambit.ru>
2017-03-01 22:21:41 +0300

    refactor

--- js/color-palet.js
+++ js/color-palet.js
@@ -214,90 +214,94 @@
 
     const colorpiker = {
         init: function() {
+            this.event();
+            this.getFile(data)
+        },
+
+        event: function() {
             let container = document.querySelector('.color-pallet-gallery');
-            let modal = document.querySelector('.color-pallet-preview__img');
-            const wrap = document.querySelector('.color-pallet-gallery-wrap');
-            let palet = document.querySelector('.color-pallet-preview');
-            const mis = document.querySelector('#fh5co-steps');
-
-
+            const mis = document.querySelector('.color-pallet-preview__close');
             const controller = document.querySelector('.pallet-gallery__controllers');
 
-
-
-            let active = false;
-            let current = 0; 
-
-            controller.addEventListener('click', (e) => {
-                let target = e.target;
-                let up = target.classList.contains('pallet-gallery__controller-up');
-                let down = target.classList.contains('pallet-gallery__controller-down');
-                let height = container.offsetHeight;
-                let heightWrap = wrap.offsetHeight;
-                
-                if(up) {
-                    current = current === 0 ? 0 : current + 200;
-                    container.style.transform = `translate3d(0,${current + 'px'},0)`;
-                }
-
-                if(down) {
-                    current = (Math.abs(current) + heightWrap) > height ? current : current - 200;
-                    container.style.transform = `translate3d(0,${current + 'px'},0)`
-                }
-            })
-
-            // function add(item) {
-            //     let current;
-            //     console.log(current)
-
-            //     return current;
-            // }
-
-            // wrap.addEventListener('mouseover', cache )
-
-            // function cache(e) {
-            //     active = true
-            //     document.body.classList.add('noscroll')
-            //     console.log(active)
-            //     if(active) {
-            //       wrap.removeEventListener('mouseover', cache)
-            //       // console.log(active)
-            //       // active = false;
-            //     }
-            // }
-            // wrap.addEventListener('mouseout', function() {
-            //     document.body.classList.remove('noscroll')
-            // } )
-
-            mis.addEventListener('click', function(e) {
-                palet.classList.remove('color-pallet-preview--active');
-                wrap.classList.remove('color-pallet-gallery-wrap--active');
-            })
-
-            container.addEventListener('click', function(e) {
-                let target = e.target;
-
-                // container.classList.add('scrollable')
-                container.classList.remove('expand')
-                palet.classList.add('color-pallet-preview--active');
-                wrap.classList.add('color-pallet-gallery-wrap--active');
-
-                // if(target.)
-                // console.log()
-                if(target.hasAttribute('src')) {
-                    let src = target.getAttribute('src');
-                    modal.style.backgroundImage = `url('${src}')`
-                    console.log(src)
-
-                }
-
-
-            })
-
-            this.getFile(data)
-
-
+            controller.addEventListener('click', this.palet.controller);
+            mis.addEventListener('click', this.palet.remove);
+            container.addEventListener('click', this.palet.add);
         },
+
+        palet: (function() {
+                let current = 0; 
+                const container = document.querySelector('.color-pallet-gallery');
+                const wrap = document.querySelector('.color-pallet-gallery-wrap');
+                let palet = document.querySelector('.color-pallet-preview');
+                let modal = document.querySelector('.color-pallet-preview__img');
+
+                return {
+                    add: function(e) {
+                        let target = e.target;
+                        let cptitpe = document.querySelector('.color-pallet-preview__title .title-key');
+                        let img = target.parentNode.classList.contains('color-pallet-gallery__img');
+                        let allImg = document.querySelectorAll('.color-pallet-gallery__img');
+                        let allImgLeng = document.querySelectorAll('.color-pallet-gallery__img').length;
+
+                        for(let i = 0; i < allImgLeng; i+=1) {
+                            allImg[i].classList.remove('active');
+                        }
+                        target.parentNode.classList.add('active')
+
+                        if(img) {
+                            container.classList.remove('expand')
+                            palet.classList.add('color-pallet-preview--active');
+                            wrap.classList.add('color-pallet-gallery-wrap--active');  
+                        }
+
+                        if (target.hasAttribute('src')) {
+                            let src = target.getAttribute('src');
+                            let title = target.parentNode.nextElementSibling.textContent;
+                            modal.style.backgroundImage = `url('${src}')`;
+                            cptitpe.textContent = title;
+                        }
+                       
+                    },
+        
+                    remove: function() {
+                        palet.classList.remove('color-pallet-preview--active');
+                        wrap.classList.remove('color-pallet-gallery-wrap--active');
+
+                        if(Math.abs(current) > container.offsetHeight) {
+                            current = -(container.offsetHeight - 360);
+                            container.style.transform = `translate3d(0,${current + 'px'},0)`;
+                        }
+                        
+
+                    },
+        
+                    controller: function(e) {
+                        let target = e.target;
+                        let up = target.classList.contains('pallet-gallery__controller-up');
+                        let down = target.classList.contains('pallet-gallery__controller-down');
+                        let height = container.offsetHeight;
+                        let heightWrap = wrap.offsetHeight;
+                        
+                        if(up) {
+                            current = current === 0 || current > 0 ? 0 : current + 200;
+                            container.style.transform = `translate3d(0,${current + 'px'},0)`;
+                        }
+        
+                        if(down) {
+                            current = (Math.abs(current) + heightWrap) > height ? current : current - 200;
+                            container.style.transform = `translate3d(0,${current + 'px'},0)`
+                        }
+                        
+                    },
+
+                    calcHeight: function() {
+                        return container.offsetHeight;
+                    }
+                }
+                    
+
+                }()),
+
 
         getFile: function(data) {
             let _data = data;
@@ -326,43 +330,3 @@
     colorpiker.init()
 
 }())
-
-
-
-    // const ColorPiker = function() {
-    //     const args = Array.prototype.slice.call(arguments);
-    //     const cb = args.pop();
-    //     let modules = (args[0] && typeof args[0]  === 'string') ? args : args[0];
-
-    //     if(!(this instanceof ColorPiker)) {
-    //         return new ColorPiker(modules, cb);
-    //     }
-
-    //     // Constructor method
-
-
-    //     if(!modules || modules === '*') {
-    //         modules = [];
-    //         for(let i in ColorPiker) {
-    //             modules.push(i);
-    //         }
-    //     }
-
-    //     for(let i = 0; i < modules.length; i += 1) {
-    //         ColorPiker.modules[modules[i]](this)
-    //     }
-
-    //     cb(this);
-    // }
-
-
-
-    // ColorPiker.modules = {};
-
-    // ColorPiker.modules.pick = (box) => {
-    //     box.color = "red";
-    // }
-
-    // ColorPiker('pick', (box) => {
-    //     console.log(box);
-    // })
